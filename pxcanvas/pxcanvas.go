5b5b594c86e52700b8abc436a9af0f071bac5bb7
Bi11y G04t <wtran292gmail.com>
2022-12-10 21:24:16 -0800

    added canvas border rendering function, canvas layout, border layout, destroy and refresh function, added CreateRenderer function to generate canvas

--- pxcanvas/pxcanvas.go
+++ pxcanvas/pxcanvas.go
@@ -6,6 +6,7 @@ import (
 	"projects/pixl/apptype"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -57,6 +58,26 @@ func NewPxCanvas(state *apptype.State, config apptype.PxCanvasConfig) *PxCanvas 
 		appState:       state,
 	}
 	pxCanvas.PixelData = NewBlankImage(pxCanvas.PxCols, config.PxRows, color.NRGBA{128, 128, 128, 255})
-	pxCanvas.ExtendBaseWidget(PxCanvas)
+	pxCanvas.ExtendBaseWidget(pxCanvas)
 	return pxCanvas
+}
+
+func (pxCanvas *PxCanvas) CreateRenderer() fyne.WidgetRenderer {
+	canvasImage := canvas.NewImageFromImage(pxCanvas.PixelData)
+	canvasImage.ScaleMode = canvas.ImageScalePixels
+	canvasImage.FillMode = canvas.ImageFillContain
+
+	canvasBorder := make([]canvas.Line, 4)
+	for i := 0; i < len(canvasBorder); i++ {
+		canvasBorder[i].StrokeColor = color.NRGBA{100, 100, 100, 255}
+		canvasBorder[i].StrokeWidth = 2
+	}
+
+	renderer := &PxCanvasRenderer{
+		pxCanvas:     pxCanvas,
+		canvasImage:  canvasImage,
+		canvasBorder: canvasBorder,
+	}
+	pxCanvas.renderer = renderer
+	return renderer
 }
