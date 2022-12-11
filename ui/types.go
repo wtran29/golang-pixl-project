9c72ab14b0dcfc324d6dedb0d667ead1d129c2b6
Bi11y G04t <wtran292gmail.com>
2022-12-10 21:54:13 -0800

    Added canvas to render on ui, made some changes to the color of swatches, included the pixlcanvas to appinit struct

--- ui/types.go
+++ ui/types.go
@@ -2,12 +2,14 @@ package ui
 
 import (
 	"projects/pixl/apptype"
+	"projects/pixl/pxcanvas"
 	"projects/pixl/swatch"
 
 	"fyne.io/fyne/v2"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
